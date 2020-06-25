64e7d630e2c32bac53a9f385201798b7caf89cc1
vargos0102 <vargos0102@gmail.com>
2020-06-25 18:09:42 -0400

    angular-added to-do application

--- June/24Jun/my-app/src/app/app.component.ts
+++ June/24Jun/my-app/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = "my-app";
+  todoArray = [];
+  todo;
+  //todoForm: new FormGroup()
+
+  addTodo(value) {
+    if (value !== "") {
+      this.todoArray.push(value);
+      //console.log(this.todos)
+    } else {
+      alert("Field required **");
+    }
+  }
+
+  /*delete item*/
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+
+  // submit Form
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoArray.push(value.todo);
+      //this.todoForm.reset()
+    } else {
+      alert("Field required **");
+    }
+  }
 }
