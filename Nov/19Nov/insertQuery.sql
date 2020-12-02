112ad8095464b0361f24884068ed188bd4bde73b
vargos0102 <vargos0102@gmail.com>
2020-12-01 19:30:00 -0500

    01Dec

---
+++ Nov/19Nov/insertQuery.sql
@@ -0,0 +1,8 @@
+INSERT INTO studentdb.students(FirstName, LastName, Email, Address)
+VALUES 
+('Tom' , 'The Cat' , 'tom@gmail.com' , 'Toronto'),
+('Jerry','The Mouse', 'jerry@gmail.com','Ottawa'),
+('Alex','V', 'alex@gmail.com','Mississauga')
+
+
+
---
+++ Nov/19Nov/students.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM studentdb.students
+LIMIT 0, 1000
+
+-- Date: 2020-11-19 19:23
+*/
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (1,'Al','Pachino','al@gmmail.com','100 City Centre Dr');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (2,'Don','Bradman','don@gmail.com','Square One Dr');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (3,'Jhonny','Bravo',NULL,'Hurontario Street');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (4,'Ted','Mosby','test@yahoo.com','John Street');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (5,'Ted','Mosby First','test@yahoo.com','New JOhn Street');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (6,'Teddy','Mosby Second','test@yahoo.com','Queen Street');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (7,'Tedif','Mosby Third','ted3@gmail.com','Niagra Street');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (13,'Tom','The Cat','tom@gmail.com','Toronto');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (14,'Jerry','The Mouse','jerry@gmail.com','Ottawa');
+INSERT INTO `` (`Id`,`FirstName`,`LastName`,`Email`,`Address`) VALUES (15,'Alex','V','alex@gmail.com','Mississauga');
